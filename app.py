7b0431e9b0e8d970bc4d3f06051afe03d594cfae
Abdalrazak Seaf Aldean <96119761+abdalrazak-seaf@users.noreply.github.com>
2024-08-26 14:39:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # """
 # Created on Sun Dec 24 19:42:21 2023
 import streamlit as st
-import page1
-import page2
+# import page1
+# import page2
 
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 
@@ -14,7 +14,7 @@ st.title('My Multi-Page Streamlit App')
 st.sidebar.title('Navigation')
 page = st.sidebar.radio('Select a Page', ['Page 1', 'Page 2'])
 
-if page == 'Page 1':
-    exec(open("page1.py").read())
-elif page == 'Page 2':
-    exec(open("page2.py").read())
+# if page == 'Page 1':
+#     exec(open("page1.py").read())
+# elif page == 'Page 2':
+#     exec(open("page2.py").read())
