ac682cee53ffc7ca16c7a090a0d0e84ab1704ddb
Abdalrazak Seaf Aldean <96119761+abdalrazak-seaf@users.noreply.github.com>
2024-09-18 11:12:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ import streamlit as st
 # import page1
 # import page2
 
+# streamlit_app.py
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
 
 
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
