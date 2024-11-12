f4372a2b399de4d1e137cbbbd3e217e0c2596e6e
Abdalrazak Seaf Aldean <96119761+abdalrazak-seaf@users.noreply.github.com>
2024-11-12 21:19:48 +0300

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -37,12 +37,53 @@ desired_order = ['Analyst-Intern', 'Future Gears Analyst', 'Analyst', 'Associate
     
     # Update the 'Designation' column to be a categorical type with the specified order
 df['Designation'] = pd.Categorical(df['Designation'], categories=desired_order, ordered=True)
-
-url2 = "https://docs.google.com/spreadsheets/d/18hn6TOgx2yRerr4tMvJgnFc0JdB0hAMK3ZT3a1azw4I/edit?pli=1&gid=1168015449#gid=1168015449"
-conn = st.connection("gsheets", type=GSheetsConnection)
-data2 = conn.read(spreadsheet=url2)
-st.dataframe(data2)
-
+    #---------------------------------
+    # First row of columns
+col1, col2, col3 = st.columns(3)
+    # Define brand colors (adjust these colors as per your brand)
+brand_colors = {
+        'Gender': '#B6AD77',  # Example blue
+        'Nationality': '#00A5CD',  # Example darker blue
+        'Branch': '#005587'  # Example even darker blue
+    }
+    
+    # Start creating Streamlit layout
+    # st.title('Distribution Metrics')
+    
+    # Gender Distribution
+with col1:
+    st.subheader('Gender Distribution')
+    gender_count = df['Gender'].value_counts()
+    fig = px.bar(gender_count, text=gender_count, color_discrete_sequence=[brand_colors['Gender']])
+    fig.update_layout(xaxis_title="Gender", yaxis_title="Count")
+    st.plotly_chart(fig, use_container_width=True)
+    
+    # Nationality Distribution
+with col2:
+    st.subheader('Nationality Distribution')
+    nationality_count = df['Nationality'].value_counts()
+    fig = px.bar(nationality_count, text=nationality_count, color_discrete_sequence=[brand_colors['Nationality']])
+    fig.update_layout(xaxis_title="Nationality", yaxis_title="Count")
+    st.plotly_chart(fig, use_container_width=True)
+    
+    # Branch Distribution
+with col3:
+    st.subheader('Branch Distribution')
+    branch_count = df['Branch'].value_counts()
+    fig = px.bar(branch_count, text=branch_count, color_discrete_sequence=[brand_colors['Branch']])
+    fig.update_layout(xaxis_title="Branch", yaxis_title="Count")
+    st.plotly_chart(fig, use_container_width=True)
+        
+   # Brand colors (repeating or selecting as necessary, ensure you have at least as many colors as designations)
+brand_colors = ['#64C8E1', '#00A5CD', '#005587', '#23282D', '#787878', 
+                    '#B6AD77', '#7AADA4', '#59BFDA', '#C8C5C6', '#787878', '#7AADA4']
+    
+    # List of designations
+designations = ['Analyst-Intern', 'Future Gears Analyst', 'Analyst', 'Associate Consultant', 'Consultant',
+                    'Manager', 'Senior Manager', 'Director', 'Principal', 'Associate Partner', 'Partner']
+    
+    # Map designations to colors
+designation_color_map = {designation: brand_colors[i] for i, designation in enumerate(designations)}
 
 
 
