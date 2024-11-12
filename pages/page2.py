dff29f128f31e4d15beb17bb9292d851fe67f925
Abdalrazak Seaf Aldean <96119761+abdalrazak-seaf@users.noreply.github.com>
2024-11-12 21:40:38 +0300

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -7,7 +7,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import random
-
 from sklearn.preprocessing import MinMaxScaler
+from streamlit_gsheets import GSheetsConnection
 
+#configure the dashboard page
+# st.set_page_config(page_title="Superstore!!!")
+    
+st.title(" :bar_chart: SG Employee Dashboard")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
+url = "https://docs.google.com/spreadsheets/d/18hn6TOgx2yRerr4tMvJgnFc0JdB0hAMK3ZT3a1azw4I/edit?pli=1&gid=0#gid=0"
+
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
 
