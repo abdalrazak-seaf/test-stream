bc177000c3d4ac0cf163de5f88e823e9c12b618e
Abdalrazak Seaf Aldean <96119761+abdalrazak-seaf@users.noreply.github.com>
2024-11-12 21:39:55 +0300

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import random
+
+from sklearn.preprocessing import MinMaxScaler
+
 
